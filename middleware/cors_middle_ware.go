5169ea1f8552b65002d8a1870fb0a100771bcebb
244t <nishishita-kyosuke@g.ecc.u-tokyo.ac.jp>
2024-11-25 01:04:55 +0900

    add log

--- middleware/cors_middle_ware.go
+++ middleware/cors_middle_ware.go
@@ -1,6 +1,9 @@
 package middleware
 
-import "net/http"
+import (
+    "net/http"
+    "fmt"
+)
 
 // CORSミドルウェア
 func CORSMiddleware(next http.Handler) http.Handler {
@@ -9,6 +12,7 @@ func CORSMiddleware(next http.Handler) http.Handler {
         w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS") // 許可するHTTPメソッド
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization") // 許可するヘッダー
 
+        fmt.Println("Handling", r.Method, "request")  // 実際のリクエストメソッド
         // OPTIONSメソッドに対しては204ステータスで即座に返す
         if r.Method == http.MethodOptions {
             w.WriteHeader(http.StatusNoContent)
