5b6cfc09f48788ae8f7091af8286a02e0d4ca4d0
244t <nishishita-kyosuke@g.ecc.u-tokyo.ac.jp>
2024-11-25 01:30:16 +0900

    add log

--- middleware/cors_middle_ware.go
+++ middleware/cors_middle_ware.go
@@ -8,10 +8,10 @@ import (
 // CORSミドルウェア
 func CORSMiddleware(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // ワイルドカード `*` は使えません
+        w.Header().Set("Access-Control-Allow-Origin", "*") 
         w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS") // 許可するHTTPメソッド
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization") // 許可するヘッダー
-        w.Header().Set("Access-Control-Allow-Credentials", "true") // 認証情報（クッキーなど）の送信を許可（必要に応じて）
+        w.Header().Set("Access-Control-Allow-Credentials", "true") 
 
         fmt.Println("Handling", r.Method, "request")  // 実際のリクエストメソッド
         // OPTIONSメソッドに対しては204ステータスで即座に返す
